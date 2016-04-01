ae95ee7edf078d20c766ecebbecc3508bad20404
mtacchino <matt@tacchino.me>
2016-04-01 16:00:04 -0400

    return a promise from translate function

--- localize.js
+++ localize.js
@@ -11,6 +11,7 @@ var Localize = (function() {
   /**
    * Translates the page using the specified language
    * @param {String} lang
+   * @returns {Promise}
    */
   function translate(lang) {
     var translationLang = lang || navigator.language || navigator.userLanguage || (navigator.languages ? navigator.languages[0] : defaultLang),
@@ -20,49 +21,55 @@ var Localize = (function() {
       Localize.currentLang = lang;
     }
 
-    getTranslations(translationLang, function(translations) {
-      for (var i = 0; i < elems.length; i++) {
-        var key = elems[i].getAttribute(keyword);
-        if (translations[key]) {
-          elems[i].innerHTML = translations[key];
+    return getTranslations(translationLang)
+      .then(function(translations) {
+        for (var i = 0; i < elems.length; i++) {
+          var key = elems[i].getAttribute(keyword);
+          if (translations[key]) {
+            elems[i].innerHTML = translations[key];
+          }
         }
-      }
-    });
+      });
   };
 
   /**
    * Retrieve translations object from a JSON file
    * @param {String} lang
-   * @param {function(Object)} cb
+   * @returns {Promise}
    */
-   function getTranslations(lang, cb) {
+   function getTranslations(lang) {
+    return new Promise(function(resolve, reject) {
+      if (translations[lang]){
+        resolve(translations[lang]);
+        return;
+      }
 
-    if (translations[lang]){
-      cb(translations[lang]);
-      return;
-    }
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", path + lang + ".json", true);
+      xhttp.send();
+      var retry = true;
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", path + lang + ".json", true);
-    xhttp.send();
-    var retry = true;
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState == 4) {
-        if (xhttp.status == 404 && retry) {
-          if (lang.length > 2) {
-            lang = lang.substring(0,2);
-            xhttp.open("GET", path + lang.substring(0,2) + ".json", true);
-            xhttp.send();
-            retry = false;
+      xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4) {
+          if (xhttp.status == 404 && retry) {
+            if (lang.length > 2) {
+              lang = lang.substring(0,2);
+              xhttp.open("GET", path + lang.substring(0,2) + ".json", true);
+              xhttp.send();
+              retry = false;
+            }
+          }
+          else if (xhttp.status == 200) {
+            translations[lang] = JSON.parse(xhttp.responseText);
+            resolve(translations[lang]);
           }
         }
-        else if (xhttp.status == 200) {
-          translations[lang] = JSON.parse(xhttp.responseText);
-          cb(translations[lang]);
-          return;
-        }
-      }
-    };
+      };
+
+      xhttp.onerror = function() {
+        reject(new Error("Error retrieving translations."));
+      };
+    });
   }
 
   translate(defaultLang);
