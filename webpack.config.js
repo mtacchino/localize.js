721d3ceecb90bae71656e1e089af14321bb1ddc8
mtacchino <matt@tacchino.me>
2017-02-21 21:10:59 +0000

    more config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 module.exports = {
+    entry: __dirname + '/src/localize.js',
     context: __dirname + '/src',
     output: {
         path: __dirname + '/dist',
